6bd27db6b61c6461d1caacba74e77894fe13fcc3
Laurence Liang <laurence.w.liang@gmail.com>
2024-03-18 15:51:36 -0400

    Add helper class

--- notebooks/src/helpers.py
+++ notebooks/src/helpers.py
@@ -4,13 +4,14 @@ class RuntimeStorage:
     def __init__(self):
         self.storage = list()
 
-    def add_entry(self):
+    def add_entry(self, original_id, model_output, task):
         tmp = dict()
         tmp['originalId'] = None
         tmp['modelOutput'] = None
+        tmp['task'] = None
         self.storage.append(tmp)
         pass
 
     def export_to_csv(self, filename):
         tmp_df = pd.DataFrame(self.storage)
-        tmp_df.to_csv(filename)
+        tmp_df.to_csv(filename, index=False)
