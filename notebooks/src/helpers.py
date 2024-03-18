cf4ead749daea32dff857fe30ea8431223622521
Laurence Liang <laurence.w.liang@gmail.com>
2024-03-18 16:18:36 -0400

    WIP

--- notebooks/src/helpers.py
+++ notebooks/src/helpers.py
@@ -4,13 +4,14 @@ class RuntimeStorage:
     def __init__(self):
         self.storage = list()
 
-    def add_entry(self, original_id, model_output, task):
+    def add_entry(self, original_id, original_input, true_label, model_output, task):
         tmp = dict()
         tmp['originalId'] = None
+        tmp['originalInput'] = None
+        tmp['trueLabel'] = None
         tmp['modelOutput'] = None
         tmp['task'] = None
         self.storage.append(tmp)
-        pass
 
     def export_to_csv(self, filename):
         tmp_df = pd.DataFrame(self.storage)
